8604aef8588612c612e7958ffa3d5de2ff90ef52
Favour Ejemole <ejemolefavour@gmail.com>
2022-02-26 17:20:58 -0600

    "TAS and TASDatabase Classes ready, Database connection established"

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.jsu.mcis.cs310.tas_sp22;
+import java.sql.*;
 
 /**
  *
  * @author user
  */
 public class TAS {
+    public static void main(String[] args){
+        TASDatabase db = new TASDatabase("tasuser","War Room D", "localhost");
+        
+        if (db.isConnected()){
+            System.err.println("Your Have Successfully Connected To The Database");
+        }
+    }
     
 }
--- src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TASDatabase.java
@@ -1,7 +1,39 @@
-
 package edu.jsu.mcis.cs310.tas_sp22;
+
+import java.sql.*;
 
 
 public class TASDatabase {
+    private final Connection connection;
+    
+    public TASDatabase(String username, String password, String address){
+        this.connection = openConnection(username, password, address);
+    }
+    
+    public boolean isConnected(){
+        boolean connected = false;
+        try {
+            if( !(connection == null))
+                connected = !(connection.isClosed());
+        }
+        catch (SQLException e) {e.printStackTrace();}
+        return connected;
+    }
+    
+    private Connection openConnection(String u, String p, String a){
+        Connection conn = null;
+        
+        if(a.equals("") || u.equals("") || p.equals(""))
+            System.err.println("ERROR: PLEASE SPECIFY THE ADDRESS/USERNAME/PASSWORD BEFORE OPENING THE DATABASE CONNECTION ");
+        else { 
+            try {
+                String url = "jdbc:mysql://" + a + "/tas_sp22_v1?autoReconnect=true&useSSL=false&zeroDateTimeBehavior=EXCEPTION&serverTimezone=America/Chicago";
+               // System.err.println("Connecting to " + url + "...");
+                conn = DriverManager.getConnection(url,u,p);
+            }
+            catch (Exception e){e.printStackTrace();}
+        }
+        return conn;
+    }
     
 }
