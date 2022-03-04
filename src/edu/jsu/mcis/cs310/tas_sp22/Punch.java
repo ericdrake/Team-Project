caadd2c494619fea657e7369f01ffc456f17fea4
Aneesh Jaiswal <ajaiswal@stu.jsu.edu>
2022-03-04 11:26:37 -0600

    updated punch class

--- src/edu/jsu/mcis/cs310/tas_sp22/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Punch.java
@@ -1,7 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Aneesh
+ */
 package edu.jsu.mcis.cs310.tas_sp22;
-
 import edu.jsu.mcis.cs310.tas_sp22.PunchType;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
