2399a2c221652061b26dbb9bf6eaffc82ffb02eb
Jasmine Allen <jaguilar@stu.jsu.edu>
2022-04-02 12:47:48 -0500

    Revert "Feature 5 completed and passed all tests"
    This reverts commit 8045809183095379f1ec8c2e33e03e90a63311d9.

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,8 +1,7 @@
 
 package edu.jsu.mcis.cs310.tas_sp22;
-
-import java.time.LocalTime;
-import java.util.ArrayList;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -14,48 +13,7 @@ public class TAS {
         
         if (db.isConnected()){
             System.err.println("Your Have Successfully Connected To The Database");
-        }   
+        }
     }
     
-    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
-        
-        int numberofPunches = dailypunchlist.size();
-        int totalMinutesWorked = 0;
-        LocalTime start = null;
-        LocalTime stop = null;
-        LocalTime lunchStart = null;
-        LocalTime lunchStop = null;
-        
-        if (numberofPunches == 1){
-            start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
-            totalMinutesWorked = start.getHour() - start.getHour();
-        }
-        
-        if (numberofPunches == 2){
-                start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
-                stop = dailypunchlist.get(1).getAdjustedtimestamp().toLocalTime();
-                totalMinutesWorked = (((stop.getHour() - start.getHour()) * 60) + (stop.getMinute() - start.getMinute()));
-                if(totalMinutesWorked > shift.getlunchthreshold()){
-                    totalMinutesWorked = (int)(totalMinutesWorked - shift.getLunchDuration());
-                }     
-        }
-        
-        if (numberofPunches == 3){
-            start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
-            lunchStart = dailypunchlist.get(3).getAdjustedtimestamp().toLocalTime();
-            lunchStop = dailypunchlist.get(3).getAdjustedtimestamp().toLocalTime();  
-        }
-        
-        if (numberofPunches == 4){
-            start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
-            stop = dailypunchlist.get(3).getAdjustedtimestamp().toLocalTime();
-            lunchStart = dailypunchlist.get(1).getAdjustedtimestamp().toLocalTime();
-            lunchStop = dailypunchlist.get(2).getAdjustedtimestamp().toLocalTime();
-            int totalLunch = ((lunchStop.getHour() - lunchStart.getHour()) + (lunchStop.getMinute() - lunchStart.getMinute()));
-            totalMinutesWorked = (((stop.getHour() - start.getHour()) * 60) + ((stop.getMinute() - start.getMinute())) - totalLunch);
-            System.err.println(dailypunchlist);
-        }
-     
-        return totalMinutesWorked;
-    }
 }
