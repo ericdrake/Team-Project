02d7d7a99e39edd106374f39205a42c0f275003c
Jasmine Allen <jaguilar@stu.jsu.edu>
2022-04-02 13:01:09 -0500

    Feature 5 completed and passed all tests (fixed)

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,7 +1,9 @@
 
 package edu.jsu.mcis.cs310.tas_sp22;
 import java.sql.*;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,4 +18,38 @@ public class TAS {
         }
     }
     
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+        
+        int numberofPunches = dailypunchlist.size();
+        int totalMinutesWorked = 0;
+        LocalTime start;
+        LocalTime stop;
+        LocalTime lunchStart;
+        LocalTime lunchStop;
+        
+        if (numberofPunches == 1){
+            start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
+            totalMinutesWorked = start.getHour() - start.getHour();
+        }
+        
+        if (numberofPunches == 2){
+                start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
+                stop = dailypunchlist.get(1).getAdjustedtimestamp().toLocalTime();
+                totalMinutesWorked = (((stop.getHour() - start.getHour()) * 60) + (stop.getMinute() - start.getMinute()));
+                if(totalMinutesWorked > shift.getlunchthreshold()){
+                    totalMinutesWorked = (int)(totalMinutesWorked - shift.getLunchDuration());
+                }     
+        }
+        
+        if (numberofPunches == 4){
+            start = dailypunchlist.get(0).getAdjustedtimestamp().toLocalTime();
+            stop = dailypunchlist.get(3).getAdjustedtimestamp().toLocalTime();
+            lunchStart = dailypunchlist.get(1).getAdjustedtimestamp().toLocalTime();
+            lunchStop = dailypunchlist.get(2).getAdjustedtimestamp().toLocalTime();
+            int totalLunch = ((lunchStop.getHour() - lunchStart.getHour()) + (lunchStop.getMinute() - lunchStart.getMinute()));
+            totalMinutesWorked = (((stop.getHour() - start.getHour()) * 60) + ((stop.getMinute() - start.getMinute())) - totalLunch);
+        }
+        
+        return totalMinutesWorked;
+    }
 }
